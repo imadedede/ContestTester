96e5f460d2852c4916a836778cd7c473d9bfee63
imadedede <imadedede@gmail.com>
2023-08-27 18:05:11 +0900

    new.sh にて、template ディレクトリ内のファイルをパスで指定されても対応できるように修正。template ディレクトリ以外のファイルを指定されてもまだ読み込めない。

--- new.sh
+++ new.sh
@@ -2,11 +2,12 @@
 
 CURRENT_DIR="$(cd "$(dirname "$0")"; pwd)"
 TEMPLATE_DIR="$CURRENT_DIR/template"
-SOURCE=$1
+FILE_NAME=${1##*/}
+ORIGIN_NAME=${FILE_NAME%.*}
 DIST=$2
 
 while read -r file
 do
     EXT=${file##*.}
     cp "$file" "$DIST.$EXT"
-done < <(find "$TEMPLATE_DIR" -name "$SOURCE.*")
+done < <(find "$TEMPLATE_DIR" -name "$ORIGIN_NAME.*")
