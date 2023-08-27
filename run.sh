f5867c42361bf1845d6a23d640fb0d5e961d0cd0
imadedede <imadedede@gmail.com>
2023-08-27 17:46:54 +0900

    run.sh にて、実行時にスクリプト自身がいるディレクトリの絶対パスを取得するように変更。それに伴いチェッカーのシェル実行部も微修正。

--- run.sh
+++ run.sh
@@ -3,13 +3,14 @@
 COMPILER=clang++
 EXT=.cpp
 
-BUILD_DIR=build
+CURRENT_DIR="$(cd "$(dirname "$0")"; pwd)"
+BUILD_DIR="$CURRENT_DIR/build"
 STD='-std=gnu++17'
 WARNING='-Wall -Wextra -fsanitize=address -fno-omit-frame-pointer'
 CHECKER=./check.py
 
 TARGET=$1
-mkdir -p $BUILD_DIR
+mkdir -p "$BUILD_DIR"
 
 eval $COMPILER "$STD" "$WARNING" -o "$BUILD_DIR/$TARGET" "$TARGET$EXT"
 $CHECKER "$BUILD_DIR/$TARGET"
