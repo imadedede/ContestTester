c5a6e02e4ebdbd0f8acc1ba119e5ba51b0ec0af3
imadedede <imadedede@gmail.com>
2023-10-07 11:41:03 +0900

    run.sh の引数に拡張子があった場合でも、拡張子以下を削った文字列をベースにソースファイルをテストケースファイルを検索するように修正。
    実装と運用が少し不格好にはなったが、実行時のサジェストでタイプ数が減る方を優先。

--- run.sh
+++ run.sh
@@ -14,7 +14,7 @@ STD='-std=gnu++17'
 WARNING='-Wall -Wextra -fsanitize=address -fno-omit-frame-pointer'
 CHECKER=./check.py
 
-TARGET=$1
+TARGET=${1%.*}
 mkdir -p "$BUILD_DIR"
 
 eval $COMPILER "$STD" "$WARNING" -o "$BUILD_DIR/$TARGET" "$TARGET$EXT"
